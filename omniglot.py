6549b2f600e135c9e1907cfb77427e8eb5141d69
RaptimusPrime <vin9jain@yahoo.co.in>
2017-06-09 14:32:31 +0200

    Add code for learning curve plots and printing output

--- omniglot.py
+++ omniglot.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from model import mann
 from utils.generators import OmniglotGenerator
+from matplotlib import pyplot as plt
 
 def omniglot():
 
@@ -45,26 +46,63 @@ def omniglot():
 
 	target_one_hot = tf.one_hot(target_var, nb_classes, axis=-1)
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output_var, labels=target_one_hot), name="cost")
-	opt = tf.train.AdamOptimizer(learning_rate=1e-3)
+	acc = tf.reduce_mean(tf.cast(tf.equal(target_var, tf.cast(tf.argmax(output_var, axis=2), dtype=tf.int32)), dtype=tf.float32))
+
+	opt = tf.train.RMSPropOptimizer(learning_rate=1e-4, decay=0.95, momentum=0.9)
 	train_step = opt.minimize(cost, var_list=params)
 
-	max_iter = 1000
+	max_iter = 100000
 
 	sess = tf.Session()
 	init = tf.global_variables_initializer()
 	sess.run(init)
 	
+	iters = []
+	accuracies = []
+	costs = []
+
 	with sess.as_default():
 		try:
 			for i in range(max_iter):
 				episode_input, episode_output = generator.episode()
 				feed_dict = {input_var: episode_input, target_var: episode_output}
 				train_step.run(feed_dict)
-				score = cost.eval(feed_dict)
-				print 'Step ' + str(i) + ': cost = ' + str(score)
+				if i % (max_iter*1e-3) == 0:
+					cost_val = sess.run(cost, feed_dict=feed_dict)
+					acc_val = sess.run(acc, feed_dict=feed_dict)
+					
+					iters.append(i)
+					costs.append(cost_val)
+					accuracies.append(acc_val)
+					
+					print 'Target Labels:'
+					print sess.run(target_var[0], feed_dict=feed_dict)
+					print 'Model Output:'
+					print sess.run(tf.argmax(output_var[0], axis=1), feed_dict=feed_dict)
+					print 'Episode ' + str(i) + ': Cost = ' + str(cost_val) + '\t Accuracy = ' + str(acc_val)
+					print ''
+
 		except KeyboardInterrupt:
-			print 'Final result --> Step ' + str(i) + ': cost = ' + score
+			print '\nInterrupted at Episode ' + str(i)
+			print 'Cost = ' + str(cost_val)
+			print 'Accuracy = ' + str(acc_val)
 			pass
+
+	
+	fig = plt.figure(figsize=(20,8))
+	plt.subplot(1,2,1)
+	plt.plot(iters, costs, 'b', label='Training Error', linewidth=2, alpha=0.8)
+	plt.xlabel('Episodes', fontsize=22)
+	plt.ylabel('Cross Entropy Loss', fontsize=22)
+	plt.title('Training Error', fontsize=26)
+
+	plt.subplot(1,2,2)
+	plt.plot(iters, accuracies, 'b-', label='Training Accuracy', linewidth=2, alpha=0.8)
+	plt.xlabel('Episodes', fontsize=22)
+	plt.ylabel('Accuracy', fontsize=22)
+	plt.title('Training Accuracy', fontsize=26)
+	plt.show()
+
 
 if __name__ == '__main__':
     omniglot()
